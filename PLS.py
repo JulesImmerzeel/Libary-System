0e9265419142e565a8ab1eb72b1e0bb5f01ce3a8
JulesImmerzeel <45227130+JulesImmerzeel@users.noreply.github.com>
2019-06-07 16:57:14 +0200

    jsonfile import

--- PLS.py
+++ PLS.py
@@ -8,18 +8,26 @@ class Person:
         self.books = []
 
 class Author(Person):
-    def __init__(self):
+    def __init__(self, name):
+        super().__init__(name)
 
-class Customer(person):
+class Customer(Person):
     def __init__(self):
+        pass
 
 
 #books
 class Book:
-    def __init__(self,title,author,ISBN):
+    def __init__(self,author,country,imageLink, language, link, pages, title, year):
+        self.author = Author(author)
+        self.country = country
+        self.imageLink = imageLink
+        self.language = language
+        self.link = link
+        self.pages = pages
         self.title = title
-        self.author = author
-        self.ISBN = ISBN
+        self.year = year
+
 
 class BookItem:
     def __init__(self,book):
@@ -29,13 +37,24 @@ class Catalog:
     def __init__(self,books):
         self.books = books
 
+
 #loaning
 class LoanAdministration:
     def __init__(self):
+        pass
 
 class LoanItem:
     def __init__(self):
+        pass
+
+
+
+test = []
+booksset1 = json.load(open('booksset1.json', 'r'))
+for books in booksset1:
+    books = Book(books['author'], books['country'], books['imageLink'], books['language'], books['link'], books['pages'], books['title'], books['year'])
+    test.append(books)
 
 #initializing the PLS
-
-catalog = Catalog([Book])
+catalog = Catalog(test)
+print(catalog.books[30].author.name)
