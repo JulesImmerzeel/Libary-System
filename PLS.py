29fd5ec8d067562a9749210b3938150af9aba1e1
ronanhub <45386974+ronanhub@users.noreply.github.com>
2019-06-13 18:10:59 +0200

    Update PLS.py
    - er is nu ook een lijst in customer class die bijhoudt welke boek elk persoon heeft
    - search returnt nu ook een list met de boeken
    - in loanAdministration geef je als argument een customer object, ipv dat er een nieuwe wordt gemaakt.
    - Bij het maken van de customers uit FakeNameSet20 wordt een nieuwe customer gemaakt en die wordt als argument gegeven in addNewCustomer

--- PLS.py
+++ PLS.py
@@ -25,6 +25,7 @@ class Customer(Person):
         self.emailAddress = emailAddress
         self.username = username
         self.telephoneNumber = telephoneNumber
+        self.myLoans = []
         super().__init__(givenName+" "+surName)
 
 
@@ -35,7 +36,8 @@ class Customer(Person):
                 if books.loaned == False:
                     books.loaned = True
                     loan_administration.allLoanedItems.append(LoanItem(self.name, books))
-                    print("Dear " + self.name + ", \nYou loaned: \nTitle: " + Book.title + "\nAuthor: " + Book.author.name + "\nLanguage: " + Book.language + "\nYear " + str(Book.year) + "\nPages: " + str(Book.pages))
+                    self.myLoans.append(books)
+                    print("\nDear " + self.name + ", \nYou loaned: \nTitle: " + Book.title + "\nAuthor: " + Book.author.name + "\nLanguage: " + Book.language + "\nYear " + str(Book.year) + "\nPages: " + str(Book.pages))
                     break
         else:
             print('all copies are loaned out')
@@ -93,9 +95,12 @@ class Catalog:
                     bookList.append(items)
                 elif str(value.lower()) == str(items.year):
                     bookList.append(items)
-        bookset = list(set(bookList)) #bookset is is een set en sets dit zorgd voor geen dubble zoekresultaten bij meerdere zoek waarde
-        for items in bookset:
-            print("\nTitle: " + items.title + "\nAuthor: " + items.author.name + "\nLanguage: " + items.language + "\nYear " + str(items.year) + "\nPages: " + str(items.pages) + "\nAvailable copies: " + str(items.getTotalCopies()))
+        bookList = list(set(bookList)) #bookset is is een set en sets dit zorgd voor geen dubble zoekresultaten bij meerdere zoek waarde
+        b = 0
+        for items in bookList:
+            print("book nr "+str(b)+"\nTitle: " + items.title + "\nAuthor: " + items.author.name + "\nLanguage: " + items.language + "\nYear " + str(items.year) + "\nPages: " + str(items.pages) + "\nAvailable copies: " + str(items.getTotalCopies()))
+        print("end of list")
+        return bookList
 
         
 
@@ -106,12 +111,12 @@ class LoanAdministration:
         self.allBookitems = []
         self.allLoanedItems = []
     
-    def addNewCustomer(self, number, gender, nameSet, givenName, surName, streetAddress, zipCode, city, emailAddress, username, telephoneNumber):
-        return self.allCustomers.append(Customer(number, gender, nameSet, givenName, surName, streetAddress, zipCode, city, emailAddress, username, telephoneNumber))
+    def addNewCustomer(self, customer):
+        return self.allCustomers.append(customer)
+
 class LoanItem:
     def __init__(self, customer, bookItem):
             self.customer = customer
-           
             self.bookItem = bookItem
 
 
@@ -137,7 +142,7 @@ for books in booksset1:
 
 # adding customers
 for people in FakeNameSet20:
-    loan_administration.addNewCustomer(people[0],people[1], people[2], people[3], people[4], people[5], people[6], people[7], people[8], people[9], people[10])
+    loan_administration.addNewCustomer(Customer(people[0],people[1], people[2], people[3], people[4], people[5], people[6], people[7], people[8], people[9], people[10]))
 
 
 print(catalog.books[10].getAvailable())
